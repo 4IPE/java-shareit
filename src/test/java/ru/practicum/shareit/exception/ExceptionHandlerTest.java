57efa240d7a6137c4e528e03e5c05ca09f3f3cf0
Daniil <99344227+4IPE@users.noreply.github.com>
2024-06-03 18:32:21 +0300

    <feat>Добавление тестов для ErrorHandler

--- src/test/java/ru/practicum/shareit/exception/ExceptionHandlerTest.java
+++ src/test/java/ru/practicum/shareit/exception/ExceptionHandlerTest.java
@@ -16,91 +16,101 @@ public class ExceptionHandlerTest {
     private ErrorHandler errorHandler;
 
     @BeforeEach
-    void setUp(){
+    void setUp() {
         this.errorHandler = new ErrorHandler();
     }
 
     @Test
-    void notFoundTest(){
-        var ex = new NotFoundException(Object.class,1);
+    void notFoundTest() {
+        var ex = new NotFoundException(Object.class, 1);
         var res = errorHandler.notFound(ex);
-        Map<String, String> exception = Map.of("NotFoundException",ex.getMessage());
+        Map<String, String> exception = Map.of("NotFoundException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void bookingFailCreateTest(){
+    void bookingFailCreateTest() {
         var ex = new BookingCreateException("bookingCreateException");
         var res = errorHandler.bookingFailCreate(ex);
-        Map<String, String> exception = Map.of("BookingCreateException",ex.getMessage());
+        Map<String, String> exception = Map.of("BookingCreateException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void notEnoughRightsExceptionTest(){
+    void notEnoughRightsExceptionTest() {
         var ex = new NotEnoughRightsException("NotEnoughRightsException");
         var res = errorHandler.bookingFailCreate(ex);
-        Map<String, String> exception = Map.of("NotEnoughRightsException",ex.getMessage());
+        Map<String, String> exception = Map.of("NotEnoughRightsException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void afterUpdateStatusnTest(){
+    void afterUpdateStatusnTest() {
         var ex = new AfterStatusUpdateException("AfterStatusUpdateException");
         var res = errorHandler.afterUpdateStatus(ex);
-        Map<String, String> exception = Map.of("AfterStatusUpdateException",ex.getMessage());
+        Map<String, String> exception = Map.of("AfterStatusUpdateException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void commentCreatedFailTest(){
+    void commentCreatedFailTest() {
         var ex = new CommentCreatedException("CommentCreatedException");
         var res = errorHandler.commentCreatedFail(ex);
-        Map<String, String> exception = Map.of("CommentCreatedException",ex.getMessage());
+        Map<String, String> exception = Map.of("CommentCreatedException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void uniqueEmailTest(){
+    void uniqueEmailTest() {
         var ex = new UniqueEmailException("UniqueEmailException");
         var res = errorHandler.uniqueEmail(ex);
-        Map<String, String> exception = Map.of("UniqueEmailException",ex.getMessage());
+        Map<String, String> exception = Map.of("UniqueEmailException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void noAvailableTest(){
+    void noAvailableTest() {
         var ex = new NoAvailableException("NoAvailableException");
         var res = errorHandler.noAvailable(ex);
-        Map<String, String> exception = Map.of("NoAvailableException",ex.getMessage());
+        Map<String, String> exception = Map.of("NoAvailableException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void dateExcTest(){
+    void dateExcTest() {
         var ex = new DateException("DateException");
         var res = errorHandler.dateExc(ex);
-        Map<String, String> exception = Map.of("DateException",ex.getMessage());
+        Map<String, String> exception = Map.of("DateException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void paramIncorrectTest(){
+    void paramIncorrectTest() {
         var ex = new ConstraintViolationException(new HashSet<>());
         var res = errorHandler.paramIncorrect(ex);
-        Map<String, String> exception = Map.of("ConstraintViolationException",ex.getMessage());
+        Map<String, String> exception = Map.of("ConstraintViolationException", ex.getMessage());
         assertThat(res).isNotNull();
         assertThat(res.values().stream().findFirst()).isEqualTo(exception.values().stream().findFirst());
     }
+
     @Test
-    void unknownStatustTest(){
+    void unknownStatustTest() {
         var ex = new NotFoundArgumentStatusException("NotFoundArgumentStatusException");
         var res = errorHandler.unknownStatus(ex);
         assertThat(res).isNotNull();
         assertThat(res.getError()).isEqualTo("Unknown state: NotFoundArgumentStatusException");
     }
+
     @Test
-    void handlerThrowableTest(){
+    void handlerThrowableTest() {
         var ex = new Throwable("handlerThrowable");
         var res = errorHandler.handlerThrowable(ex);
         assertThat(res).isNotNull();
